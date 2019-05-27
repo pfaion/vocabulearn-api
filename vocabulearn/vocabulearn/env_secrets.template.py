ea82c7040fa3bc3b11de69dfdb3d65556ed11d34
Patrick Faion <p.faion@web.de>
2019-05-27 23:29:47 +0200

    Clean django project with setup for secret Heroku DB credentials

---
+++ vocabulearn/vocabulearn/env_secrets.template.py
@@ -0,0 +1,16 @@
+# This file stores sensitive information that should not get synced to public code repositories. It
+# also allows for running different configurations between local debugging vs production. You can
+# either copy this file to 'env_secrets.py' and fill in the empty values, or define local
+# environment variables in the same fashion. The application will look for the 'env_secrets.py'
+# first.
+
+secrets = dict(
+    # your django secret key
+    SECRET_KEY='',
+    # you probably want that to be 1 for local debugging, 0 for actual production
+    DJANGO_DEBUG=1,
+    # your database url (postgresql)
+    DATABASE_URL='',
+    # localhost for local debugging, actual hostname otherwise
+    ALLOWED_HOSTS='localhost',
+)
---
+++ vocabulearn/vocabulearn/settings.py
@@ -0,0 +1,114 @@
+import os
+import dj_database_url
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# private/sensitive information that will not be put into git
+DEBUG = False
+SECRET_KEY = ''
+DATABASES = dict()
+ALLOWED_HOSTS = []
+
+try:
+    from .env_secrets import secrets
+except ModuleNotFoundError:
+    secrets = os.environ
+
+if 'DJANGO_DEBUG' in secrets:
+    DEBUG = secrets['DJANGO_DEBUG'] in [1, '1']
+
+if 'SECRET_KEY' in secrets:
+    SECRET_KEY = secrets['SECRET_KEY']
+else:
+    print('Error: Secret key not specified!')
+
+if 'DATABASE_URL' in secrets:
+    DATABASES['default'] = dj_database_url.parse(secrets['DATABASE_URL'])
+else:
+    print('Error: Database url not specified!')
+
+if 'ALLOWED_HOSTS' in secrets:
+    ALLOWED_HOSTS = secrets['ALLOWED_HOSTS']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'vocabulearn.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'vocabulearn.wsgi.application'
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_URL = '/static/'
